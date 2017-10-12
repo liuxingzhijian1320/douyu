31b4f5c6793fb99a2eae7778c8c0e55ab39771c1
liuxingzhijian1320 <zhs1320@163.com>
2017-10-12 16:40:03 +0800

    init

---
+++ src/pages/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mr-root">
+      <public-header>
+        <p class="title">个人中心</p>
+      </public-header>
+      <div class="mr-content">
+        用户
+      </div>
+    </div>
+</template>
+
+<script>
+  import PublicHeader from '../components/Header'
+  export default {
+    components:{
+      PublicHeader
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
